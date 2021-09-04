c1b3f89afeb7bf50c217af96b734112ccf22bc76
Geovani de Souza <geovanisouza92@gmail.com>
2021-09-04 20:19:36 -0300

    silence sass deprecation notices for carbon
    Signed-off-by: Geovani de Souza <geovanisouza92@gmail.com>

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  sassOptions: {
+    quietDeps: true,
+  },
 };
 
 module.exports = withNx(nextConfig);
